3ef5aff3a96b71e617d18a283ba15a9d7b91f401
greataspirationmy <58585758+greataspirationmy@users.noreply.github.com>
2024-11-27 17:10:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,6 @@ const urlsToCache = [
   '/greataspirationmy.github.io/gamember/events.html',
   '/greataspirationmy.github.io/gamember/manifest.json',
   '/greataspirationmy.github.io/gamember/supabase.js',
-  '/greataspirationmy.github.io/gamember/js/app.js',
   '/greataspirationmy.github.io/gamember/icon-144x144.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css', 
   // 添加其他需要缓存的资源
@@ -21,17 +20,19 @@ self.addEventListener('install', event => {
 });
 
 // 激活Service Worker
-self.addEventListener('activate', event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // 单独处理每个请求，忽略失败的请求
+        return Promise.all(
+          urlsToCache.map(url => {
+            return cache.add(url).catch(err => {
+              console.log('Failed to cache:', url, err);
+            });
+          })
+        );
+      })
   );
 });
 
